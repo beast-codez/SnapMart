5caede703ede36ed491e01056f095c218455d62b
beast-codez <lalithvedium456@gmail.com>
2025-01-27 01:40:30 +0530

    changed the backend url

--- Frontend/src/Buy/HandleBuy.jsx
+++ Frontend/src/Buy/HandleBuy.jsx
@@ -20,7 +20,7 @@ const handleBuy = async (amount, userDetails, buyItems) => {
 
     // Send amount and user details to the backend to create an order
     const { data } = await axios.post(
-      "http://localhost:5000/api/payment/order",
+      "https://snapmart-9loi.onrender.com/api/payment/order",
       { amount },
       { withCredentials: true }
     );
@@ -45,7 +45,7 @@ const handleBuy = async (amount, userDetails, buyItems) => {
         try {
           // Handle successful payment
           const verifyResponse = await axios.post(
-            "http://localhost:5000/api/payment/verify",
+            "https://snapmart-9loi.onrender.com/api/payment/verify",
             {
               razorpay_payment_id: response.razorpay_payment_id,
               razorpay_order_id: response.razorpay_order_id,
@@ -56,7 +56,7 @@ const handleBuy = async (amount, userDetails, buyItems) => {
 
           if (verifyResponse.data.success) {
             await axios.post(
-              "http://localhost:5000/addOrder",
+              "https://snapmart-9loi.onrender.com/addOrder",
               { buyItems },
               { withCredentials: true }
             );
